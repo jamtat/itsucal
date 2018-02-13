3dd1c681eb33fe3d52b624ff7bac51d092d12aba
Ben Tattersley <ubershinysheep@gmail.com>
2018-02-13 21:17:16 +0000

    Add tests for ISBN13 equality

--- common/src/test/kotlin/moe/itsu/common/model/ISBN13Test.kt
+++ common/src/test/kotlin/moe/itsu/common/model/ISBN13Test.kt
@@ -9,4 +9,15 @@ class ISBN13Test {
         val isbn = ISBN13("978-1-947804-02-9")
         assertEquals("9781947804029", isbn.toString())
     }
+
+    @Test
+    fun checkSameISBN() {
+        val a = ISBN13("978-1-947804-02-9")
+        val b = ISBN13("978-1-947804-02-9")
+        val c = ISBN13("9781947804029")
+        assertEquals(a, b)
+        assertEquals(a, c)
+        assertEquals(b, c)
+        assertEquals(a, a)
+    }
 }
